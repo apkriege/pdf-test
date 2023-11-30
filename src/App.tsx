59ba26bbd46cff8889c12d9a3aaf5071458899ed
unknown <adamkrieger87@gmail.com>
2023-11-29 21:49:28 -0500

    test2

--- src/App.tsx
+++ src/App.tsx
@@ -2,51 +2,45 @@
 // import solidLogo from './assets/solid.svg'
 // import viteLogo from '/vite.svg'
 
+
 import { jsPDF } from "jspdf";
 
 import './App.css'
 
 function App() {
-  // const [count, setCount] = createSignal(0)
-
-  const pdf = document.getElementById('.pdf');
-  // const htmlpdf = () => {
-  //   const element = document.getElementById('pdf');
-  //   const opt = {
-  //     margin:       1,
-  //     filename:     'myfile.pdf',
-  //     image:        { type: 'jpeg', quality: 0.98 },
-  //     html2canvas:  { scale: 2 },
-  //     jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
-  //   };
-
-
-  //   // html2pdf().from(element).set(opt).save();
-  // }
+  const pdf = document.getElementById("pdf");
 
   const downloadPDF = () => {
-    const doc = new jsPDF();
-    doc.text("Hello world!", 10, 10);
-    doc.save("a4.pdf");
+    const doc = new jsPDF({
+      format: "a4",
+      unit: "px",
+      orientation: 'p'
+    });
+    doc.setFontSize(10)
+    doc.html(pdf.current, {
+      async callback(doc) {
+        await window.open(URL.createObjectURL(doc.output("blob")));
+      },
+    });
   }
 
   return (
     <>
       <div id="pdf">
         <div class="bg-white text-black p-10 h-[500px] w-[400px]">
-          <h1>TESTING PDF</h1>
+          <h1 class="">TESTING PDF</h1>
         </div>
-  
-      </div>
-      
-      <div class="mt-5">
-      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2" onClick={downloadPDF}>
-        JSPDF
-      </button>
 
-      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-        Button
-      </button>
+      </div>
+
+      <div class="mt-5">
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2" onClick={downloadPDF}>
+          JSPDF
+        </button>
+
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Button
+        </button>
       </div>
 
 
